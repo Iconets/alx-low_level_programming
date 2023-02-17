c7b144f73fc699cfb403481111da447957b67f95
iconets <iconetsenterprise>
2023-02-16 16:17:41 -0800

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print a string
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%5", "with proer grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
