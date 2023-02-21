844c5b841144982687c7f61d2f0edcdc06036230
iconets <iconetsenterprise>
2023-02-21 10:13:16 -0800

    0-putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,26 @@
+#ifndef main_h
+#define main_h
+
+/**
+ * _putchar - Writes the character c to std
+ * @c: Parameter to compare
+ */
+int _putchar(char c);
+
+/**
+ * print_alphabet - function to print alphabet
+ *
+ */
+void print_alphabet(void);
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ */
+void print_alphabet_x10(void);
+
+
+
+
+
+#endif
