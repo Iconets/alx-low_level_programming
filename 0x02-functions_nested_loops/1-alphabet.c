d6f0913993bd3a5884b08880f9bd6a7c24d19376
iconets <iconetsenterprise>
2023-02-21 13:16:40 -0800

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - -print alphabet
+ *
+ * Return: Void
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
