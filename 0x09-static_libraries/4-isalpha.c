3a26d52c49752b541c510d2b7913600e227ca320
iconets <iconetsenterprise>
2023-03-10 18:24:10 -0800

    update

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _isalpha - Entry point
+ * Description: checks is a character is lowercase
+ * @c: the integer value it receives
+ * Return: 1 if true. 0 if false.
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+the read me information for static lib
